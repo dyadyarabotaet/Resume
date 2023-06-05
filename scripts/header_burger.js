24733d3d63f4d950336d3c636a2d3a50bc748c15
dyadyarabotaet <dyadyaadihaet@gmail.com>
2023-06-05 13:05:48 +0700

    changes in the b-menu script

--- scripts/header_burger.js
+++ scripts/header_burger.js
@@ -5,11 +5,13 @@ var burgerMenu = document.querySelector(".header-burger");
 checkboxOpen.addEventListener("change", function () {
   if (checkboxOpen.checked) {
     burgerMenu.style.top = "0";
+    checkboxClose.checked = false;
   }
 });
 
 checkboxClose.addEventListener("change", function () {
   if (checkboxClose.checked) {
     burgerMenu.style.top = "-750px";
+    checkboxOpen.checked = false;
   }
 });
