5b71f9ef9ac0c10919aae113b21169e4326e8cb1
dyadyarabotaet <dyadyaadihaet@gmail.com>
2023-06-05 12:57:08 +0700

    added header burger script

---
+++ scripts/header_burger.js
@@ -0,0 +1,15 @@
+var checkboxOpen = document.getElementById("toggle-burger");
+var checkboxClose = document.getElementById("toggle-burger-off");
+var burgerMenu = document.querySelector(".header-burger");
+
+checkboxOpen.addEventListener("change", function () {
+  if (checkboxOpen.checked) {
+    burgerMenu.style.top = "0";
+  }
+});
+
+checkboxClose.addEventListener("change", function () {
+  if (checkboxClose.checked) {
+    burgerMenu.style.top = "-750px";
+  }
+});
