30d16379f058560a6fc108aaec33a8325bde9b92
dyadyarabotaet <dyadyaadihaet@gmail.com>
2023-06-05 13:29:27 +0700

    Update header_burger.js

--- scripts/header_burger.js
+++ scripts/header_burger.js
@@ -1,3 +1,4 @@
+// Opening and closing menu
 var checkboxOpen = document.getElementById("toggle-burger");
 var checkboxClose = document.getElementById("toggle-burger-off");
 var burgerMenu = document.querySelector(".header-burger");
@@ -15,3 +16,14 @@ checkboxClose.addEventListener("change", function () {
     checkboxOpen.checked = false;
   }
 });
+
+// Closing menu after navigation
+
+var links = document.querySelectorAll(".menu-list-item a");
+
+for (var link of links) {
+  link.addEventListener("click", function () {
+    burgerMenu.style.top = "-750px";
+    checkboxOpen.checked = false;
+  });
+}
