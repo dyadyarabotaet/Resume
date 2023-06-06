6cd71f81f0247e2585db583169cfb3d059f4c232
dyadyarabotaet <dyadyaadihaet@gmail.com>
2023-06-06 11:49:32 +0700

    Update scrolling_higher.js

--- scripts/scrolling_higher.js
+++ scripts/scrolling_higher.js
@@ -1,14 +1,27 @@
 // Scrolling little higher than href
 
 function scrollToSection(event, target, offset) {
-  event.preventDefault(); // Предотвращает стандартное поведение ссылки
+  event.preventDefault();
 
-  const section = document.querySelector(target);
-  if (section) {
-    const topOffset = section.getBoundingClientRect().top + window.pageYOffset;
-    window.scrollTo({
-      top: topOffset + offset,
-      behavior: "smooth", // Добавление плавной прокрутки
-    });
+  if (window.matchMedia("(min-width: 791px)").matches) {
+    const section = document.querySelector(target);
+    if (section) {
+      const topOffset =
+        section.getBoundingClientRect().top + window.pageYOffset;
+      window.scrollTo({
+        top: topOffset + offset,
+        behavior: "smooth",
+      });
+    }
+  } else {
+    const section = document.querySelector(target);
+    if (section) {
+      const topOffset =
+        section.getBoundingClientRect().top + window.pageYOffset;
+      window.scrollTo({
+        top: topOffset,
+        behavior: "smooth",
+      });
+    }
   }
 }
