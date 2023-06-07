b91b5edbc245acb24e470c9faf353965dee7c144
dyadyarabotaet <dyadyaadihaet@gmail.com>
2023-06-07 13:27:05 +0700

    trying to make filter menu

--- scripts/my_works.js
+++ scripts/my_works.js
@@ -1,11 +1,30 @@
-var menuItems = document.querySelectorAll(".my-works-menu-item");
+const filterButtons = document.querySelectorAll(".my-works-menu-item");
+const items = document.querySelectorAll(".projects-list-item");
 
-for (var menuItem of menuItems) {
-  menuItem.addEventListener("click", function () {
-    for (var item of menuItems) {
-      item.classList.remove("selected");
+for (var filterButton of filterButtons) {
+  filterButton.addEventListener("click", function () {
+    for (var button of filterButtons) {
+      button.classList.remove("selected");
     }
 
     this.classList.add("selected");
+  });
+}
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", filterItems);
+});
+
+function filterItems(e) {
+  const selectedCategory = e.target.getAttribute("data-category");
+
+  items.forEach((item) => {
+    const itemCategory = item.getAttribute("data-category");
+
+    if (selectedCategory === "all" || selectedCategory === itemCategory) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
   });
 }
