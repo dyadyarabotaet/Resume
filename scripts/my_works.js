5410aa14dabefd9f98d2170fcbc82a0bea35f67c
dyadyarabotaet <dyadyaadihaet@gmail.com>
2023-06-07 16:34:30 +0700

    Made it English

--- scripts/my_works.js
+++ scripts/my_works.js
@@ -1,25 +1,22 @@
 const filterButtons = document.querySelectorAll(".my-works-menu-item");
 const items = document.querySelectorAll(".projects-list-item");
 
-for (var filterButton of filterButtons) {
-  filterButton.addEventListener("click", function () {
-    for (var button of filterButtons) {
-      button.classList.remove("selected");
-    }
-
-    this.classList.add("selected");
-  });
-}
-
 filterButtons.forEach((button) => {
   button.addEventListener("click", filterItems);
 });
 
 function filterItems(e) {
-  const selectedCategory = e.target.getAttribute("data-category");
+  console.log("e.currentTarget", e.currentTarget);
+  for (var button of filterButtons) {
+    button.classList.remove("selected");
+  }
 
+  e.currentTarget.classList.add("selected");
+  const selectedCategory = e.currentTarget.getAttribute("data-category");
+  console.log("selectedCategory", selectedCategory);
   items.forEach((item) => {
     const itemCategory = item.getAttribute("data-category");
+    console.log("itemCategory", itemCategory);
 
     if (selectedCategory === "all" || selectedCategory === itemCategory) {
       item.style.display = "flex";
