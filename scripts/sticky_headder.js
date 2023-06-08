00e5ab6ef5ddf3ae673b69fe0c59ec721db5ca42
dyadyarabotaet <dyadyaadihaet@gmail.com>
2023-06-08 17:18:31 +0700

    some simple fixes

--- scripts/sticky_headder.js
+++ scripts/sticky_headder.js
@@ -1,9 +1,19 @@
 window.addEventListener("scroll", function () {
   var header = document.querySelector(".sticky-header");
+  var themeButton = document.querySelector(".toggle-theme-label");
+  var burgerOpen = document.querySelector(".burger-open");
 
   if (window.pageYOffset > 0) {
-    header.classList.remove("blured-backdrop");
+    header.classList.remove("transparent-background");
   } else {
-    header.classList.add("blured-backdrop");
+    header.classList.add("transparent-background");
+  }
+
+  if (window.pageYOffset > 530) {
+    themeButton.classList.remove("transparent-background");
+    burgerOpen.classList.remove("transparent-background");
+  } else {
+    themeButton.classList.add("transparent-background");
+    burgerOpen.classList.add("transparent-background");
   }
 });
